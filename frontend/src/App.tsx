4b59cee83d13d516ef20f5e72885d64d06be98ea
bhatia-di <bhatia.di@northeastern.edu>
2022-11-15 03:45:49 -0500

    added changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './styles/App.css';
 
 import { Header } from './components/Header';
 import { UserListComponent } from './components/UserList';
